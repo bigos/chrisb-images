c7b330d884cfe39a5e0dca6b40e1f05b829334c0
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-14 00:48:19 +0000

    created new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChrisbImages::Application.config.secret_token = 'fc31a56e881a4a86ba26b93618494aeff075baf51d74acb0faaaa02f39729414c168748f323b5f5796280f5f933737af1c3b1cfb7000d30e2f6c7b66d24defd5'
