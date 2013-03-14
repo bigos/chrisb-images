d29aa02ae4668821fbbd2322fe7816225d68d17b
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-14 15:42:14 +0000

    tidied up ckeditor and added comment scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 ChrisbImages::Application.routes.draw do
 
+  resources :comments
+
+
+  resources :posts
+
+
   match 'login' => 'user_sessions#create'
   match 'logout' => 'user_sessions#destroy'
   resource :user_session
   resource :account, :controller => "users"
   
   mount Ckeditor::Engine => '/ckeditor'
+
+  root :to => 'posts#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +64,7 @@ ChrisbImages::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'user_session#new'
+  # root :to => 'user_session#new'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314103530) do
+ActiveRecord::Schema.define(:version => 20130314154128) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -28,6 +28,20 @@ ActiveRecord::Schema.define(:version => 20130314103530) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], :name => "idx_ckeditor_assetable"
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], :name => "idx_ckeditor_assetable_type"
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "user_sessions", :force => true do |t|
     t.datetime "created_at", :null => false
