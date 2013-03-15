42a367562b919045d1880ca8dca683fe74747850
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-15 17:43:32 +0000

    found way to check if I am logged in, but I'm not sure if it is correct approach

--- features/step_definitions/sample_authentication_steps.rb
+++ features/step_definitions/sample_authentication_steps.rb
@@ -1,18 +1,62 @@
-
-
-# This will guess the User class
-ll='leonard'
-user=User.new(:login => ll, :password => ll, :password_confirmation=>ll)
-
-p user
+require 'factory_girl'
 
 Given(/^the following user exists:$/) do |table|
+  data = table.raw[1]
+  #p data
+  # This will guess the User class
+  FactoryGirl.define do
+    factory :user do
+      login data[0]
+      password data[1]
+      password_confirmation data[1]
+    end
+  end
+  user = FactoryGirl.create(:user)  
+  #p user
+end
 
-  
-  # table is a Cucumber::Ast::Table
-  data = table.raw
-  puts data.inspect
-  #p User.all
+Given(/^I am not logged in$/) do
+  UserSession.find.try(:destroy)
+  #pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the homepage$/) do
   pending # express the regexp above with the code you wish you had
 end
 
+When(/^I follow "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I fill in the following:$/) do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be on the user page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should not see "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am logged in as "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given(/^I am on the user page$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^I should be on the homepage$/) do
+  pending # express the regexp above with the code you wish you had
+end
--- features/support/authlogic.rb
+++ features/support/authlogic.rb
@@ -1,9 +1,8 @@
-
-require "authlogic"
 require "authlogic/test_case"
+
 World(Authlogic::TestCase)
 
-#ApplicationController.skip_before_filter :activate_authlogic
+ApplicationController.skip_before_filter :activate_authlogic
 
 Before do
   activate_authlogic
