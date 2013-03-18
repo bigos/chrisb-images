073c03844677d86f25f255857886145b3de4d451
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-18 00:37:33 +0000

    added sending of welcome email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      # Tell the UserMailer to send a welcome Email after save
+      UserMailer.welcome_email(@user).deliver
       flash[:notice] = "Account registered!"
       redirect_back_or_default account_url
     else
