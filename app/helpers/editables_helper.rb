9ff40f817b9df3c276835739555969f44ab4067f
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-20 23:36:22 +0000

    now only admin can see links to edit editable content

--- app/helpers/editables_helper.rb
+++ app/helpers/editables_helper.rb
@@ -1,6 +1,10 @@
 module EditablesHelper
   def editable_content(name)
     editable = Editable.where(:name => name).first
-    "<div id=\"#{editable.name}\">\n#{editable.content}\n</div>"
+    if current_user     
+      edit_link = link_to("Edit", edit_editable_path(editable)) if current_user.login == 'admin'
+    end
+    "<div id=\"#{editable.name}\">\n#{editable.content}\n</div>#{edit_link}"
+
   end
 end
