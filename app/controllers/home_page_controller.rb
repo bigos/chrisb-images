5b036c76f3b37bdc4706c475e851525a88dba538
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-20 22:45:03 +0000

    successfully added editable content on home page

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,4 +1,7 @@
 class HomePageController < ApplicationController
   def index
+    @big_photo = Editable.where(:name => "big_photo").first
+    logger.info @big_photo.inspect
+    
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318005350) do
+ActiveRecord::Schema.define(:version => 20130320222558) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(:version => 20130318005350) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
+    t.text     "content"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "editables", :force => true do |t|
+    t.string   "name"
     t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
