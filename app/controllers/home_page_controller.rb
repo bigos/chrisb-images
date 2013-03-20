3ca3b1be43da60336d3bf7ea4b4c532ab229ee25
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-20 23:15:37 +0000

    success, editables work as expected

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -1,7 +1,8 @@
 class HomePageController < ApplicationController
   def index
     @big_photo = Editable.where(:name => "big_photo").first
-    logger.info @big_photo.inspect
+    #logger.info @big_photo.inspect
+    @featured_image = Editable.where(:name => "featured_image").first
     
   end
 end
--- app/helpers/editables_helper.rb
+++ app/helpers/editables_helper.rb
@@ -1,2 +1,6 @@
 module EditablesHelper
+  def editable_content(name)
+    editable = Editable.where(:name => name).first
+    "<div id=\"#{editable.name}\">\n#{editable.content}\n</div>"
+  end
 end
