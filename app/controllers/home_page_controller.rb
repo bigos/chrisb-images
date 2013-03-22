3a7e763e82cc86a789a19cea1e042e8308084f20
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-22 21:32:50 +0000

    done most of the footer area

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -3,6 +3,10 @@ class HomePageController < ApplicationController
     @big_photo = Editable.where(:name => "big_photo").first
     #logger.info @big_photo.inspect
     @featured_image = Editable.where(:name => "featured_image").first
-    
+    @posts = Post.find(:all, :order => "created_at DESC",:limit => 5)
+
+
+
+
   end
 end
