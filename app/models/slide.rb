8be3e89309b17760000f536cadca449da3335613
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-25 20:25:36 +0000

    added slideshow slides

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,3 +1,4 @@
 class Slide < ActiveRecord::Base
-  attr_accessible :caption, :position, :visible
+  attr_accessible :caption, :position, :visible, :photo
+  has_attached_file :photo, :styles => { :thumb => "190x90>" }
 end
