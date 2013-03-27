0bb5fe97f5c7699d2db13e5154400dc284957540
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-27 14:47:32 +0000

    added drop down menu for workshop type

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,3 +1,7 @@
 class Workshop < ActiveRecord::Base
   attr_accessible :content, :teaser, :title, :type
+  validates_presence_of  :content, :teaser, :title, :type
+  
+  TYPE_OPTIONS =  ['weekend', '3 day', '5 day']
+  validates :type, :inclusion => { :in =>  TYPE_OPTIONS, :message => "%{value} is not a valid option" }
 end
--- spec/models/workshop_spec.rb
+++ spec/models/workshop_spec.rb
@@ -1,9 +1,34 @@
 require 'spec_helper'
+require 'factory_girl'
 
 describe Workshop do
   it "has a valid factory" do
-    w = FactoryGirl.create(:workshop)
-    p w
-    w.should be_valid
+    workshop = FactoryGirl.create(:workshop)
+    workshop.should be_valid
+  end
+  it "is invalid without a type" do
+    FactoryGirl.build(:workshop, type: nil).should_not be_valid
+  end
+  it "is invalid without a title" do
+    FactoryGirl.build(:workshop, title: nil).should_not be_valid
+  end
+  it "is invalid without a teaser" do
+    FactoryGirl.build(:workshop, teaser: nil).should_not be_valid
+  end
+  it "is invalid without a title" do
+    FactoryGirl.build(:workshop, teaser: nil).should_not be_valid
+  end
+
+  it "is invalid with wrong type" do
+    FactoryGirl.build(:workshop, type: 'rrrr_hourly').should_not be_valid
+  end
+  it "is valid with type - weekend" do
+    FactoryGirl.build(:workshop, type: 'weekend').should be_valid
+  end
+  it "is valid with type - 3 day" do
+    FactoryGirl.build(:workshop, type: '3 day').should be_valid
+  end
+  it "is valid with type - 5 day" do
+    FactoryGirl.build(:workshop, type: '5 day').should be_valid
   end
 end
