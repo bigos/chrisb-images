cdb20f3c59ee3a3823a92948c21ce41d81491463
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-29 17:34:22 +0000

    started testing tags and photos

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -2,8 +2,8 @@
 
 FactoryGirl.define do
   factory :photo do
-    attachment_file_name "MyString"
-    attachment_content_type "MyString"
+    attachment_file_name "MyString.jpg"
+    attachment_content_type "image/jpeg"
     attachment_file_size 1
     attachment_updated_at "2013-03-28 13:18:04"
   end
