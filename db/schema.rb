d72e47c7d4912a9cea02686a666e4b877080fdd2
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-29 17:18:17 +0000

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130328131804) do
+ActiveRecord::Schema.define(:version => 20130329171433) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -70,6 +70,12 @@ ActiveRecord::Schema.define(:version => 20130328131804) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "user_sessions", :force => true do |t|
