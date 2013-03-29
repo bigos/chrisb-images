9bd72392ab47a84b894773eaedcb8d18969903cb
Jacek Podkanski <ruby.object@googlemail.com>
2013-03-29 01:52:52 +0000

    got back to upload form with an error

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -44,12 +44,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { 
-          render :json => [@photo.to_jq_attachment].to_json,
-          :content_type => 'text/html',
-          :layout => false
-
-          #redirect_to @photo, notice: 'Photo was successfully created.' 
+        format.html { redirect_to photos_path, notice: 'Photo was successfully created.' 
         }
         format.json { render json: @photo.to_jq_upload, status: :created, location: @photo }
       else
