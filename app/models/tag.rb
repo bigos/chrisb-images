bc269eb925a64e7df3e9a8106850d5246f9fe8e3
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-01 21:55:12 +0100

    added parent_id

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,11 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :parent_id
   
   has_many :taggings
   has_many :photos, :through => :taggings
+
+  has_many :children, :class_name => "Tag", :foreign_key => "parent_id"
+  belongs_to :parent, :class_name => "Tag"
   
   validates :name, :presence => true, :uniqueness => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130331205841) do
+ActiveRecord::Schema.define(:version => 20130401205128) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(:version => 20130331205841) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "parent_id"
   end
 
   create_table "user_sessions", :force => true do |t|
