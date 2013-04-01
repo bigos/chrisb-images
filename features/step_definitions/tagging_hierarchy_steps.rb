8b01717153383cf83f32d0530cf81a7bfb259119
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-01 22:35:35 +0100

    got basic relationship working

--- features/step_definitions/tagging_hierarchy_steps.rb
+++ features/step_definitions/tagging_hierarchy_steps.rb
@@ -1,11 +1,17 @@
 When(/^I put tag "(.*?)" as a child of "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  parent = Tag.where(:name => arg2).first
+  child = Tag.where(:name => arg1).first
+  child.parent_id = parent.id
+  child.save
+  child.parent_id.should eq(parent.id)
 end
 
 Then(/^Tag "(.*?)" should have "(.*?)" as the parent$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  child = Tag.where(name: arg1).first
+  child.parent.name.should eq(arg2)
 end
 
 Then(/^Tag "(.*?)" should have "(.*?)" as one of it's children$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
+  parent = Tag.where(name: arg1).first
+  parent.children.collect{|c| c.name}.should include(arg2)
 end
