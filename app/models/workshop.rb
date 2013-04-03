6f3a345e43709f013b3955740b651faa6aeba295
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-03 22:54:29 +0100

    removed teaser and added start date validation

--- app/models/workshop.rb
+++ app/models/workshop.rb
@@ -1,6 +1,6 @@
 class Workshop < ActiveRecord::Base
-  attr_accessible :content, :teaser, :title, :duration, :start_date
-  validates_presence_of  :content, :teaser, :title, :duration
+  attr_accessible :content, :title, :duration, :start_date
+  validates_presence_of  :content, :title, :duration, :start_date
   
   DURATION_OPTIONS = {'weekend' => 2, '3 day' => 3, '5 day'=> 5}
   validates :duration, :inclusion => { :in => DURATION_OPTIONS.values, :message => "%{value} is not a valid workshop duration" }
