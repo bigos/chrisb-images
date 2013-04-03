a8510287ad333293820599929d337b52e83eb6b0
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-03 13:41:18 +0100

    added max_level for traversing tag tree

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,24 +1,27 @@
 module ApplicationHelper
 
-  def recursive_tag_links(tag_name)
+  def recursive_tag_links(tag_name, level, max_level)
     tag = Tag.where(:name=> tag_name).first
     str = ''
     if tag.children
-      str << '<ul>'
-      tag.children.each do |tc|
-        str << "<li>#{link_to tc.name, tc} #{Photo.includes(:tags).where(:tags => {:name => tc.name}).count}   #{recursive_tag_links(tc.name)}</li>\n"
+      if level <= max_level
+        
+        str << '<ul>'
+        tag.children.each do |tc|
+          str << "<li>#{link_to tc.name + level.to_s, tc} #{Photo.includes(:tags).where(:tags => {:name => tc.name}).count}   #{recursive_tag_links(tc.name, level+1, max_level)}</li>\n"
+        end
+        str << '</ul>'
       end
-      str << '</ul>'
     end
   end
 
-  def tag_links
+  def tag_links(max_level = 1000)
     top_tags = Tag.where(:parent_id => nil).all
     str = ''
     
     str << "<ul>"
     top_tags.each do |tt|
-      str << "<li>#{link_to tt.name, tt} #{Photo.includes(:tags).where(:tags => {:name => tt.name}).count}  #{recursive_tag_links(tt.name)}</li>\n"
+      str << "<li>#{link_to tt.name, tt} #{Photo.includes(:tags).where(:tags => {:name => tt.name}).count}  #{recursive_tag_links(tt.name, 1, max_level)}</li>\n"
     end
     str << "</ul>"
     
