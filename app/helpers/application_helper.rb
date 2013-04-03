01571186044cd923e6b3268548cd7e9bdb8a9cdd
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-03 16:25:53 +0100

    refactored the helper and got links to photos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,36 +4,39 @@ module ApplicationHelper
     Photo.includes(:tags).where(:tags => {:name => tag.name}).count
   end
 
-  def tag_li(tc, increase,level, max_level)
+  def tag_li(tc, increase,level, max_level, type)
     str =  "<li>"
-    str << "<!- level #{level} #{max_level} -->"
-    str << "#{link_to tc.name, tc}"
-    str << " #{tagged_count tc}"
-    str << " #{recursive_tag_links(tc.name, level+increase, max_level)}"
+    if type == :tags
+      str << link_to( tc.name, tc)
+    else
+      str << link_to(tc.name, photos_path(:tag => tc.name))
+    end
+    str << tagged_count( tc)
+    str << recursive_tag_links(tc.name, level+increase, max_level, type).to_s
     str << "</li>\n"
   end
 
-  def recursive_tag_links(tag_name, level, max_level)
+  def recursive_tag_links(tag_name, level, max_level,type)
     tag = Tag.where(:name=> tag_name).first
     str = ''
     if tag.children
       if level <= max_level        
         str << "<ul>"
         tag.children.each do |tc|
-          str << tag_li(tc, 1, level, max_level)
+          str << tag_li(tc, 1, level, max_level,type)
         end
         str << '</ul>'
       end
     end
   end
 
-  def tag_links( max_level = 1000)
+  def tag_links( max_level = 1000, type = :tags)
     top_tags = Tag.where(:parent_id => nil).all
     level = 1
     str = ''    
     str << "<ul>"
     top_tags.each do |tc|
-      str << tag_li(tc, 0, level, max_level)
+      str << tag_li(tc, 0, level, max_level, type)
     end
     str << "</ul>"
   end
