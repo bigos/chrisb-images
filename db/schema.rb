a43433bca06be5ee542514636bb4cbae9e5bcca1
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-06 00:21:22 +0100

    added photo_id to tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130403214734) do
+ActiveRecord::Schema.define(:version => 20130405231931) do
 
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20130403214734) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "parent_id"
+    t.integer  "photo_id"
   end
 
   create_table "user_sessions", :force => true do |t|
