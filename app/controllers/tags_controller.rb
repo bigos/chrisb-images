f21ed3d575487a55d92ca6659b6bff7b84373040
Jacek Podkanski <ruby.object@googlemail.com>
2013-04-05 01:28:50 +0100

    showing untagged photos - will probably need a proper sql query here

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -16,7 +16,10 @@ class TagsController < ApplicationController
   def show
     @tag = Tag.find(params[:id])
     @photos = Photo.includes(:tags).where(:tags => {:name => @tag.name})
-    @untagged_photos = Photo.all - @photos
+    @untagged_photos = []
+    Photo.all.each do |p|
+      @untagged_photos << p if p.tags.count == 0
+    end
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @tag }
